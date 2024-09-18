4c3d0ba69d7cbfbea33a63824638338236fb0bec
ViNx3h <vinhphuochongnnn@gmail.com>
2024-09-18 15:05:25 +0700

    test

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,6 +10,19 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+    }),
+  Author: a
+    .model({
+      nameAuthor: a.string().required(),
+      Description: a.string(),
+      id: a.id(),
+    }),
+  Book: a
+    .model({
+      nameBook: a.string().required(),
+      price: a.float(),
+      author: a.ref('Author')
+
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -32,7 +45,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import { generateClient } from "aws-amplify/data";
+import { FormEvent, useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [authors, setAuthors] = useState<Array<Schema["Author"]["type"]>>([]);
+  const [books, setBooks] = useState<Array<Schema["Book"]["type"]>>([]);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -13,27 +17,81 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    client.models.Author?.observeQuery()?.subscribe({
+      next: (data) => setAuthors([...data.items])
+    });
+  }, []);
+
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  interface CreateAuthorFormElements extends HTMLFormControlsCollection {
+    authorName: HTMLInputElement;
+    authorDes: HTMLInputElement;
+  }
+
+  interface CreateAuthorForm extends HTMLFormElement {
+    readonly elements: CreateAuthorFormElements;
+  }
+
+
+  const handleCreate = async (event: FormEvent<CreateAuthorForm>) => {
+
+    event.preventDefault();
+    const form = event.currentTarget;
+    const nameAuthor = form.elements.authorName.value;
+    const desAuthor = form.elements.authorDes.value;
+    console.log(nameAuthor);
+    console.log(desAuthor);
+
+    await client.models?.Author?.create({
+      nameAuthor: nameAuthor,
+      Description: desAuthor,
+    })
+    // console.log(isCreate);
+  }
+
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+
+    <Authenticator>
+      {({ signOut }) => (
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <button onClick={signOut}>Sign out</button>
+          <div>
+            Authors:
+            <form onSubmit={handleCreate}>
+              <label htmlFor="authorName">AuthorName: </label>
+              <input type="text" id="authorName" name="authorName" />
+              <label htmlFor="authorDes">Description: </label>
+              <input type="text" id="authorDes" name="authorDes" />
+              <input type="submit" value="Create" />
+            </form>
+            <br />
+            <ul>
+              {authors.map((author) => (
+                <li key={author.id}>
+                  <div>{author.nameAuthor}</div>
+                  <br />
+                  <div>{author.Description}</div>
+                </li>
+              ))
+
+              }
+            </ul>
+          </div>
+        </main>
+
+      )}
+    </Authenticator>
   );
 }
 
